bf53ee8afd5e3d26d325dc4a8e5b1137e0d5779a
Joe Mankin <joseaphmankin@gmail.com>
2018-10-16 10:19:18 -0500

    Final touches and chalking. Added more input validation

--- Word.js
+++ Word.js
@@ -1,45 +1,34 @@
+//WORD Constructor for the individual word. Only dependant is Letter. 
+
 let Letter = require('./Letter');
 
 class Word {
     constructor(word) {
         this.word = word;
-        // An array of new Letter objects representing the letters of the underlying word
+        // Various arrays to hold a the secret word split up, "new Letter" objects representing the 
+        // letters of the underlying word, guessed Letters and the "DISPLAY"
         this.splitLetters = [];
         this.convertedLtrs = []
         this.guessedLtrs = [];
         this.display =[];
     }
 
-    //Loops the splitLetter string created by index, gives each one the NEW LETTER class, pushes those objects
+    //Only ran once. Loops the splitLetter string created by index, gives each one the NEW LETTER class, pushes those objects
     //to the convertedLtrs array, and then fills the display with the underscores using the ltr.switcher method.
     convert(){
         for (let i = 0; i < this.splitLetters.length; i++){
             // let ltr = this.splitLetters[i];
             // console.log(ltr);
             var ltr = new Letter(this.splitLetters[i]);
+            if(ltr.letter === " "){
+                ltr.guessedYet = true;
+            }
             this.convertedLtrs.push(ltr);
             this.display.push(ltr.switcher());
         }
         // console.log("CONVERTED: " + this.convertedLtrs[0].letter)
-        console.log(this.display.join(" "))
+        console.log(this.display.join(" ") + "\n")
     }
-
-// A function that returns a string representing the word. 
-//This should call the function on each letter object (the first function defined in Letter.js) 
-//that displays the character or an underscore and concatenate those together.
-
-    // stringer(){
-    //     this.display = [];
-    //     for (let i = 0; i < this.convertedLtrs.length; i++){
-    //         // let ltr = this.splitLetters[i];
-    //         // console.log(ltr);
-    //         var ltr = new Letter(this.splitLetters[i]);
-    //         // console.log(ltr);
-    //         this.display.push(ltr.switcher());
-    //     }
-    //     console.log(this.display.join(" "));
-
-    // }
 
 // A function that takes a character as an argument and calls the guess function on each letter object 
 //(the second function defined in Letter.js)
@@ -47,10 +36,6 @@ class Word {
     guesser(guess){
         let ltr = guess.toUpperCase()
         this.display = [];
-
-        // if(this.splitLetters.includes(ltr)){
-        //     console.log ("It's in there")
-        // };
 
             for (let i = 0; i < this.convertedLtrs.length; i++){
             
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+//Main Game logic. Only dependant on Word.  
+
 let Word = require("./Word");
 let inquirer = require("inquirer");
 let chalk = require("chalk");
 
-let words = ["DRACULA", "WOLFMAN", "MUMMY"];
+let words = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 let guessedLetters = []
 let secretWord = {};
 let tries = 10;
 
+console.log(chalk.blue(`
+*************************************
+** WELCOME TO 50 STATES WORD GUESS **
+*************************************
+`))
+
+//kicks game loop off. 
 wordGenerator();
 
 // Randomly selects a word and uses the Word constructor to store it
@@ -21,6 +30,7 @@ function wordGenerator() {
 	getGuess();
 }
 
+
 function getGuess() {
 	// Prompts the user for each guess and keeps track of the user's remaining guesses
 	inquirer.prompt([
@@ -29,7 +39,7 @@ function getGuess() {
 			message: "Please Guess a Letter",
 			validate: function (value) {
 				var letters = /^[A-Za-z]+$/;
-				if (value.match(letters)) {
+				if (value.match(letters) && value.length === 1) {
 					return true;
 				}
 				else {
@@ -61,7 +71,7 @@ function getGuess() {
 function winChecker() {
 
 	if (!secretWord.display.includes("_")) {
-		console.log(chalk.green("You Got it!! The Secret Word was: " + secretWord.word));
+		console.log(chalk.blue("You Got it!! The Secret Word was: " + secretWord.word));
 		playAgain()
 	} else if (tries > 0) {
 		console.log("You've guessed: " + guessedLetters.join(", "))
